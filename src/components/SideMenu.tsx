e7f5530dfb2b4d13691dcefbc147a044d678ec1b
Pedro Costa <pedro4costa@outlook.com>
2026-01-07 18:28:59 -0300

    Enhance side menu styling; improve title appearance and link interactions

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -4,6 +4,7 @@ import CountriesList from './CountriesList'
 const SideMenu = () => {
   return (
     <nav className="side-menu">
+      <h2 className="side-title">Menu</h2>
       <NavLink className="link" to="/countries">
         Países
       </NavLink>
